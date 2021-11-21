fbca7533f263c46fa6993c4f173b2655eccdb3d3
Fernando Nunes <fernandonunesjs@hotmail.com>
2021-11-21 17:18:07 -0300

    End

--- useeefect/src/App.js
+++ useeefect/src/App.js
@@ -1,11 +1,24 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(0);
+  count = new Date()
+
+  useEffect(() => {
+    document.title = `Clock ${count.toLocaleTimeString()}`
+  },[count]);
+
+  setInterval(() => {
+    setCount(count = new Date())
+  },1000)
+   
   return (
-    <div className="App">
-     
+    <div className='clock'>
+      <p>{count.toLocaleTimeString()}</p>
     </div>
-  );
+  )
+  
 }
 
 export default App;
